246633846b7486353727115f7232c88464627934
atulkalantre <88487294+atulkalantre@users.noreply.github.com>
2022-12-31 12:41:57 +0530

    VaccineInfo Class to Func componemt

--- src/Project/ParentVaccineInfo.js
+++ src/Project/ParentVaccineInfo.js
@@ -1,80 +1,65 @@
 import axios from 'axios';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import AdminHome from './AdminHome';
 import { Route, BrowserRouter } from 'react-router-dom';
 import ParentHome from './ParentHome';
 
-export default class ParentVaccineInfo extends React.Component{
+const ParentVaccineInfo = (props) => {
+    const [vaccine, setVaccine] = useState([])
 
-
-    constructor(props){
-        super(props)
-        this.state ={
-            vaccine:[]
-            
-        }
-    }
-
-    componentDidMount=()=>
-    {  
-
+    useEffect(() => {
         axios
-        .get("http://localhost:8080/getallvaccine")
-        .then(res=>{
-      //      alert( res);
-            const json=JSON.parse(JSON.stringify(res.data));
-        this.setState({vaccine:json});
-     //   alert(this.state.info);
-        });        
+            .get("http://localhost:8080/getallvaccine")
+            .then(res => {
+                const json = JSON.parse(JSON.stringify(res.data));
+                setVaccine(json);
+            });
+    }, [])
+
+    const goback = () => {
+        props.history.push("/parenthome");
     }
-    goback = () => {
-        this.props.history.push("/parenthome");
-    }
 
-   
+    return (
+        <div>
 
-    render(){
-        return(
-            <div>
+            <h3>Vaccine List</h3>
 
-                <h3>Vaccine List</h3>
+            <table className="table table-bordered table-striped table-dark ">
+                <tr>
+                    <th>VACCINE ID</th>
+                    <th>VACCINE NAME</th>
+                    <th>DESCRIPTION</th>
+                    <th>BRAND</th>
+                    <th>TYPE</th>
+                    <th>PRICE</th>
+                    <th>AGE RANGE</th>
 
-                <table className="table table-bordered table-striped table-dark ">
-                    <tr>
-                        <th>VACCINE ID</th>
-                        <th>VACCINE NAME</th>
-                        <th>DESCRIPTION</th>
-                        <th>BRAND</th>
-                        <th>TYPE</th>
-                        <th>PRICE</th>
-                        <th>AGE RANGE</th>
-                        
-                    </tr>             
-                    {this.state.vaccine.map(v =>
-                    
+                </tr>
+                {vaccine.map(v =>
+
                     <tr>
                         <td>{v.vid}</td>
                         <td>{v.vname}</td>
                         <td>{v.vdesc}</td>
                         <td>{v.brand}</td>
                         <td>{v.type}</td>
-                        <td>{v.price}</td>   
-                        <td>{v.agerange}</td>                     
+                        <td>{v.price}</td>
+                        <td>{v.agerange}</td>
                     </tr>
-                )}            
-           </table> 
-           <button type="submit" class="btn btn-info   col-md-2"onClick={this.goback}>Go Back</button><br/>
-           <h4><b>#Note :-</b> <br></br> co = Compulsory.  <br></br> op = optional.  </h4>
+                )}
+            </table>
+            <button type="submit" class="btn btn-info   col-md-2" onClick={goback}>Go Back</button><br />
+            <h4><b>#Note :-</b> <br></br> co = Compulsory.  <br></br> op = optional.  </h4>
 
-           <BrowserRouter>
-                    <div>
-                        <Route path="/parenthome" component={ParentHome}></Route>
-                    </div>
-           </BrowserRouter>
+            <BrowserRouter>
+                <div>
+                    <Route path="/parenthome" component={ParentHome}></Route>
+                </div>
+            </BrowserRouter>
 
 
-            </div>
-        )
-    }
-
+        </div>
+    )
 }
+export default ParentVaccineInfo
